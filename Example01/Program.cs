d927f8e2d36549b675a2873fd7a70d9cfac94383
newhand2017 <chivalry19850@gmail.com>
2017-03-14 19:51:20 +0800

    ENUM example

---
+++ Example01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Status response = Status.Error;
+
+            switch (response)
+            {
+                case Status.Success:
+                    //需要的程式
+                    Console.WriteLine("狀態是成功");
+                    Console.WriteLine((int)response);
+                    break;
+
+                case Status.Error:
+                    //需要的程式
+                    Console.WriteLine("狀態是失敗");
+                    Console.WriteLine((int)response);
+                    break;
+
+                default:
+                    //需要的程式
+                    Console.WriteLine("狀態是不明，什麼都不做");
+                    Console.WriteLine(response);
+                    break;
+            }
+        }
+    }
+}
